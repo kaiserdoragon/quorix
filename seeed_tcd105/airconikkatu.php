e3bdc0923f4ccbef8646b204acf7c4d527262f24
QUORIX\quori <kaiserdoragon1121@gmail.com>
2025-12-24 14:28:23 +0900

    【エアコン一括見積LP】最低限の作業環境差作成

--- seeed_tcd105/airconikkatu.php
+++ seeed_tcd105/airconikkatu.php
@@ -6,6 +6,14 @@ Template Name: エアコン見積り一括LP
 
 <?php get_header(); ?>
 
+<header>
+    架空の見積サイト名をヘッダーに入れて、電話番号とメールのボタンを設置
+</header>
+
 <p>エアコン見積り一括LPの内容が入ります</p>
+
+<footer>
+    会社概要はないので、サイトの運営者情報を「QUORIX」で掲載しておく
+</footer>
 
 <?php get_footer(); ?>
